eec5decacaa81aed89d7d177dcee9095eab1ad2a
Nathan Kronenfeld <nkronenfeld@uncharted.software>
2017-10-04 16:18:01 -0400

    Test to make sure alternative testing styles (BDD-styles) actually work.

---
+++ core/src/test/scala/org/apache/spark/rdd/RDDFlatSpec.scala
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.spark.rdd
+
+import org.scalatest.FlatSpec
+
+import org.apache.spark.SharedSparkContext
+
+class RDDFlatSpec extends FlatSpec with SharedSparkContext {
+  "An RDD" should "have the specified number of partitions" in {
+    assert(1 === sc.makeRDD(Array(1, 2, 3, 4), 1).getNumPartitions)
+    assert(2 === sc.makeRDD(Array(1, 2, 3, 4), 2).getNumPartitions)
+    assert(3 === sc.makeRDD(Array(1, 2, 3, 4), 3).getNumPartitions)
+    assert(4 === sc.makeRDD(Array(1, 2, 3, 4), 4).getNumPartitions)
+  }
+
+  it should "have the correct element count, no matter how many partitions it has" in {
+    assert(4 === sc.makeRDD(Array(1, 2, 3, 4), 1).count)
+    assert(4 === sc.makeRDD(Array(1, 2, 3, 4), 2).count)
+    assert(4 === sc.makeRDD(Array(1, 2, 3, 4), 3).count)
+    assert(4 === sc.makeRDD(Array(1, 2, 3, 4), 4).count)
+  }
+
+  "An RDD with duplicate elements" should "eliminate duplicates with distinct" in {
+    assert(4 === sc.makeRDD(Array(1, 1, 2, 2, 3, 3, 4, 4), 1).distinct.count)
+    assert(4 === sc.makeRDD(Array(1, 1, 2, 2, 3, 3, 4, 4), 2).distinct.count)
+    assert(4 === sc.makeRDD(Array(1, 1, 2, 2, 3, 3, 4, 4), 3).distinct.count)
+    assert(4 === sc.makeRDD(Array(1, 1, 2, 2, 3, 3, 4, 4), 4).distinct.count)
+  }
+}
---
+++ sql/core/src/test/scala/org/apache/spark/sql/SQLGenericContextWordSpec.scala
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql
+
+import org.scalatest.WordSpec
+
+import org.apache.spark.sql.test.SharedSQLGenericContext
+
+class SQLGenericContextWordSpec extends WordSpec with SharedSQLGenericContext {
+  import testImplicits._
+
+  "A Simple Dataset" when {
+    val ds = Seq((1, 1), (2, 1), (3, 2), (4, 2), (5, 3), (6, 3), (7, 4), (8, 4)).toDS
+    "looked at as complete rows" should {
+      "have the specified number of elements" in {
+        assert(8 === ds.count)
+      }
+      "have the specified number of unique elements" in {
+        assert(8 === ds.distinct.count)
+      }
+    }
+    "refined to specific columns" should {
+      "have the specified number of elements in each column" in {
+        assert(8 === ds.select("_1").count)
+        assert(8 === ds.select("_2").count)
+      }
+      "have the correct number of distinct elements in each column" in {
+        assert(8 === ds.select("_1").distinct.count)
+        assert(4 === ds.select("_2").distinct.count)
+      }
+    }
+  }
+}
